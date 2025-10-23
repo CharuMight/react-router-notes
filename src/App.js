3add65285d12c05fa2aa1fac6190646aa61f16bc
CharuMight <88588859+CharuMight@users.noreply.github.com>
2025-10-23 15:04:13 +0000

    no match route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import About from './components/About';
 import NavBar from './components/NavBar';
 import OrderSummary from './components/OrderSummary';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -11,10 +12,12 @@ function App() {
       <NavBar />
 
       <Routes>
+
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />} />
 
+        <Route path='*' element={<NotFound />}></Route> 
       </Routes>
       
     </div>
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <div>
+            Page not found ❌
+        </div>
+    )
+};
+
+export default NotFound;
