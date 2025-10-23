d429812206bd0719b7d4ca2fc3d33a5d063f315f
CharuMight <88588859+CharuMight@users.noreply.github.com>
2025-10-23 14:23:15 +0000

    lint fixes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import About from './components/About';
 function App() {
   return (
     <div className="App">
+
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
+      
     </div>
   );
 }
