8fffe027afb60c9e9bdc000966f79b6fa06344d5
CharuMight <88588859+CharuMight@users.noreply.github.com>
2025-10-23 16:17:14 +0000

    useParams hook

--- src/components/users/UserDetails.jsx
+++ src/components/users/UserDetails.jsx
@@ -1,8 +1,13 @@
+import { useParams } from "react-router-dom";
+
 const UserDetails = () => {
+    const params = useParams();
+    const userId = params.userId;
+
     return (
         <>
             <div>UserDetails Page</div>
-            <div>More details</div>
+            <div>Details about {userId}</div>
         </>
     );
 };
