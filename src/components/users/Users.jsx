df8a2a43378536bbefd4be12bbdba833729f8797
CharuMight <88588859+CharuMight@users.noreply.github.com>
2025-10-23 16:26:07 +0000

    search params for bookmark and shareable page link"

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,6 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const Users = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    
+    const getActiveUsers = () => {
+        setSearchParams({filter: "active"})
+    };
+
+    const resetFilter = () => {
+        setSearchParams({});
+    };
+
+    const showActiveUsers = searchParams.get("filter") === 'active';
+
     return (
         <div>
             <h3>User 1</h3>
@@ -8,6 +21,13 @@ const Users = () => {
             <h3>User 3</h3>
 
             <Outlet />
+
+            <div>
+                <button onClick={getActiveUsers}>Active users</button>
+                <button onClick={resetFilter}>Reset filter</button>
+            </div>
+
+            {showActiveUsers ? <h3>List of Active users</h3> : null}
         </div>
     );
 };
